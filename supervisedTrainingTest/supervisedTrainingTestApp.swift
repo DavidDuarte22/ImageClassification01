af3f20b3f5efd81f5f0bb12f4aac56ed7b91a3be
David Duarte <david.duarte@endava.com>
2020-10-06 15:29:15 -0300

    Initial Commit

---
+++ supervisedTrainingTest/supervisedTrainingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  supervisedTrainingTestApp.swift
+//  supervisedTrainingTest
+//
+//  Created by itsupport on 06/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct supervisedTrainingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
