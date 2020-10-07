a754ebe4e1ddbe5d752943c9684569de4fc2f2e8
David Duarte <david.duarte@endava.com>
2020-10-07 14:37:46 -0300

    Adding UI files, selecting photos from library or using the camera

--- supervisedTrainingTest/supervisedTrainingTestApp.swift
+++ supervisedTrainingTest/supervisedTrainingTestApp.swift
@@ -15,3 +15,11 @@ struct supervisedTrainingTestApp: App {
         }
     }
 }
+
+#if DEBUG
+struct supervisedTrainingTestApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
+#endif
